d9d2460f968669fed1abfc89740b465ca3d82aa0
Piero <piero.onorati@virgilio.it>
2021-07-28 15:31:57 +0200

    init

---
+++ database/db.php
@@ -0,0 +1,67 @@
+<?php
+
+$records =[
+    [
+        'title' => "Heard It in a Past Life",
+        'author' => "Maggie Rogers",
+        'year' => 2019,
+        'poster' => "https://static.billboard.com/files/media/maggie-rogers-heard-it-in-a-past-life-album-art-2019-billboard-1240-compressed.jpg"
+    ],
+    [
+        'title' => "Forever Neverland",
+        'author' => "MØ",
+        'year' => 2018,
+        'poster' => "https://static.billboard.com/files/media/mo-forever-neverland-album-art-2018-billboard-embed-compressed.jpg"
+    ],
+    [
+        'title' => "Melodrama",
+        'author' => "Lorde",
+        'year' => 2016,
+        'poster' => "https://static.billboard.com/files/media/03-lorde-melodrama-album-art-2017-billboard-1240-compressed.jpg"
+    ],
+    [
+        'title' => "Pure Comedy",
+        'author' => " Father John Misty",
+        'year' => 2017,
+        'poster' => "https://static.billboard.com/files/media/father-john-misty-pure-comedy-2017-billboard-embed-compressed.jpg"
+    ],
+    [
+        'title' => "Currents",
+        'author' => "Tame Impala",
+        'year' => 2015,
+        'poster' => "https://static.billboard.com/files/media/22-Tame-Impala-Currents-best-album-art-2015-billboard-1500-compressed.jpg"
+    ],
+    [
+        'title' => "Surrender",
+        'author' => "Hurts",
+        'year' => 2015,
+        'poster' => "https://static.billboard.com/files/media/12-Hurts-Surrender-best-album-art-2015-billboard-1500-compressed.jpg"
+    ],
+    [
+        'title' => "LP1",
+        'author' => "FKA Twigs",
+        'year' => 2014,
+        'poster' => "https://static.billboard.com/files/media/FKA-Twigs-LP1-album-covers-billboard-1000x1000-compressed.jpg"
+    ],
+    [
+        'title' => "Born in the U.S.A.",
+        'author' => "Bruce Springsteen",
+        'year' => 1984,
+        'poster' => "https://static.billboard.com/files/media/Bruce-Springsteen-Born-in-the-USA-album-covers-billboard-1000x1000-compressed.jpg"
+    ],
+    [
+        'title' => "Unknown Pleasures",
+        'author' => "Joy Division",
+        'year' => 1979,
+        'poster' => "https://static.billboard.com/files/media/Joy-Division-Unknown-Pleasures-album-covers-billboard-1000x1000-compressed.jpg"
+    ],
+    [
+        'title' => "Maggot Brain",
+        'author' => "Funkadelic",
+        'year' => 1971,
+        'poster' => "https://static.billboard.com/files/media/Funkadelic-Maggot-Brain-album-covers-billboard-1000x1000-compressed.jpg"
+    ]
+
+]
+
+?>
